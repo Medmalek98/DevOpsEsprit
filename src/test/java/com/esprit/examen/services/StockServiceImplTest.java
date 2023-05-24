6aa31ca563f54c16ef2b7904de6039a2f1afe65b
Medmalek98 <95756360+Medmalek98@users.noreply.github.com>
2023-05-24 17:44:29 +0100

    Update StockServiceImplTest.java

--- src/test/java/com/esprit/examen/services/StockServiceImplTest.java
+++ src/test/java/com/esprit/examen/services/StockServiceImplTest.java
@@ -9,6 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import com.esprit.examen.entities.Stock;
 
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class StockServiceImplTest {
