07957607e978c9cad7b1d86e9d87bceb8a111fef
Medmalek98 <95756360+Medmalek98@users.noreply.github.com>
2023-05-24 17:38:59 +0100

    Update SecteurActivite.java

--- src/main/java/com/esprit/examen/entities/SecteurActivite.java
+++ src/main/java/com/esprit/examen/entities/SecteurActivite.java
@@ -22,6 +22,7 @@ public class SecteurActivite implements Serializable{
 	/**
 	 * 
 	 */
+	
 	private static final long serialVersionUID = 1L;
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
