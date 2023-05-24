a2869f94b06517f3bc955ba68159d81d8092671f
Medmalek98 <95756360+Medmalek98@users.noreply.github.com>
2023-05-24 18:10:26 +0100

    Update IFactureService.java

--- src/main/java/com/esprit/examen/services/IFactureService.java
+++ src/main/java/com/esprit/examen/services/IFactureService.java
@@ -7,6 +7,7 @@ import com.esprit.examen.entities.Facture;
 public interface IFactureService {
 	List<Facture> retrieveAllFactures();
 
+	
 	List<Facture> getFacturesByFournisseur(Long idFournisseur);
 
 	Facture addFacture(Facture f);
